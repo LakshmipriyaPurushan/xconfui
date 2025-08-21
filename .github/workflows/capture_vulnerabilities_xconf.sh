0e345855a47c6c684cd050c9116aca1b9362e380
LakshmipriyaPurushan <60867402+LakshmipriyaPurushan@users.noreply.github.com>
2025-08-21 14:05:05 +0530

    Update capture_vulnerabilities_xconf.sh

--- .github/workflows/capture_vulnerabilities_xconf.sh
+++ .github/workflows/capture_vulnerabilities_xconf.sh
@@ -27,7 +27,11 @@ go install golang.org/x/vuln/cmd/govulncheck@latest
 export PATH=$PATH:$(go env GOPATH)/bin
 
 REPO_URL="https://github.com/rdkcentral/xconfui.git"
-git -c advice.detachedHead=false clone --branch v1.0.2 --depth 1 "$REPO_URL"
-cd  xconfui
+
+TAG=v1.0.2
+git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO_URL" xconfui || \
+  (echo "Shallow clone failed, retrying full clone..." && git -c advice.detachedHead=false clone --branch "$TAG" "$REPO_URL" xconfui)
+  
+cd xconfui
 govulncheck ./... >  ~/xconf_vulnerability_reports/xconfui_govulncheck.txt
 
